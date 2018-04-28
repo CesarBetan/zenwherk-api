5ff2dbcb404c50fd0c083ea7c4b5fd21ba797ce5
David Zarate <davidzaratetrujillo@gmail.com>
2018-04-28 14:45:24 -0500

    Add Authentication

--- src/main/java/com/zenwherk/api/ApiApplication.java
+++ src/main/java/com/zenwherk/api/ApiApplication.java
@@ -2,11 +2,24 @@ package com.zenwherk.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends ResourceServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/v1/public/**").permitAll()
+				.antMatchers("/**").authenticated();
 	}
 }
