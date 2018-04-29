9b74153da77b426e1047dd395afeecdaaff2ec6a
David Zarate <davidzaratetrujillo@gmail.com>
2018-04-29 17:05:54 -0500

    Remove endpoint scan from jersey

--- src/main/java/com/zenwherk/api/config/JerseyConfig.java
+++ src/main/java/com/zenwherk/api/config/JerseyConfig.java
@@ -2,6 +2,7 @@ package com.zenwherk.api.config;
 
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.zenwherk.api.endpoint.*;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,26 @@ import org.springframework.stereotype.Component;
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(JacksonJaxbJsonProvider.class);
-        packages("com.zenwherk.api.endpoint");
+        registerEndpoints();
         property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+    }
+
+    public void registerEndpoints() {
+        register(FavoriteEndpoint.class);
+        register(PictureEndpoint.class);
+        register(PlaceChangeEndpoint.class);
+        register(PlaceEndpoint.class);
+        register(PlaceEndpointPublic.class);
+        register(PlaceFeatureChangeEndpoint.class);
+        register(PlaceFeatureEndpoint.class);
+        register(PlaceFeatureEndpointPublic.class);
+        register(PlaceScheduleChangeEndpoint.class);
+        register(PlaceScheduleEndpoint.class);
+        register(PlaceScheduleEndpointPublic.class);
+        register(ReviewEndpoint.class);
+        register(ReviewEndpointPublic.class);
+        register(StatsEndpoint.class);
+        register(UserEndpoint.class);
+        register(UserEndpointPublic.class);
     }
 }
