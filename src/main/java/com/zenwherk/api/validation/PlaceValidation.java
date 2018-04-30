36b04a9fbe08e0c5f3d68c3806ce3a4fc445d3bb
David Zarate <davidzaratetrujillo@gmail.com>
2018-04-30 16:57:46 -0500

    Change phone validation

--- src/main/java/com/zenwherk/api/validation/PlaceValidation.java
+++ src/main/java/com/zenwherk/api/validation/PlaceValidation.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class PlaceValidation {
 
     public static final Pattern VALID_PHONE_REGEX =
-            Pattern.compile("^[+]+[0-9]{10,12}$", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[+]+[0-9]{10,13}$", Pattern.CASE_INSENSITIVE);
     public static Result<Place> validateInsert(Place place) {
         Result<Place> result = new Result<>();
         result.setErrorCode(null);
